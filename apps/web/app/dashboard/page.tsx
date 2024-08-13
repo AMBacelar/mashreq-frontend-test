78f0ade323082bb563be0d02b6d070ff1adddefc
Adilson Bacelar <a.m.bacelar@gmail.com>
2024-08-13 23:57:04 +0100

    sample content for web login

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,37 +1,101 @@
-/* eslint-disable @next/next/no-img-element */
-"use client";
-
+"use client"
+import Link from "next/link"
+import {
+  CircleUser,
+  Menu,
+  Package2,
+  Search,
+} from "lucide-react"
 import { Button } from "../../components/ui/button"
+import { Sheet, SheetContent, SheetTrigger } from "../../components/ui/sheet"
+import { Input } from "../../components/ui/input"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../../components/ui/dropdown-menu"
+import { PageContent } from "./page-content"
 
-export const Login = () => {
+const navLinks = [
+  { href: "#", label: "Dashboard" },
+  { href: "#", label: "Orders" },
+  { href: "#", label: "Products" },
+  { href: "#", label: "Customers" },
+  { href: "#", label: "Analytics" },
+];
+
+export default function Dashboard() {
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen">
-      <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-        <div className="flex items-center justify-center py-12">
-          <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Dashboard</h1>
-              <p className="text-balance text-muted-foreground">
-                You should be logged in now with your details available
-              </p>
-            </div>
-            <Button type="submit" className="w-full">
-              Log out
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <Link
+            href="#"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+          >
+            <Package2 className="h-6 w-6" />
+            <span className="sr-only">Acme Inc</span>
+          </Link>
+          {navLinks.map((link, i) => (
+            <Link key={link.label} href={link.href} className={`${i === 0 ? 'text-foreground ' : 'text-muted-foreground '}transition-colors hover:text-foreground`}>
+              {link.label}
+            </Link>
+          ))}
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
             </Button>
-          </div>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <nav className="grid gap-6 text-lg font-medium">
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
+                <Package2 className="h-6 w-6" />
+                <span className="sr-only">Acme Inc</span>
+              </Link>
+              {navLinks.map((link, i) => (
+                <Link key={link.label} href={link.href} className={`${i !== 0 && 'text-muted-foreground '}hover:text-foreground`}>
+                  {link.label}
+                </Link>
+              ))}
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+          <form className="ml-auto flex-1 sm:flex-initial">
+            <div className="relative">
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search products..."
+                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+              />
+            </div>
+          </form>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-        <div className="hidden bg-muted lg:block">
-          <img
-            src="https://placeholder.pics/svg/300"
-            alt="Image"
-            width="1920"
-            height="1080"
-            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          />
-        </div>
-      </div>
-    </main>
+      </header>
+      <PageContent />
+    </div>
   )
 }
-
-export default Login
